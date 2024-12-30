1cc8d1a9e3481f9b8fdf188c12dcb150e63102e1
anmol420 <anmolanand2k15@gmail.com>
2024-12-30 17:14:48 +0530

    Changes In Cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
 
 app.use(express.json({
     limit: "16kb"
